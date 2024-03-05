d838070f0e21d64891ced0f2a804e1ad92414e21
margesh <tanmay.dukhande@softlabsgroup.in>
2024-03-05 12:11:54 +0530

    add components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Hello from './components/Hello';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello This is auto deploy</h1>
+      <Hello/>
     </div>
   );
 }
